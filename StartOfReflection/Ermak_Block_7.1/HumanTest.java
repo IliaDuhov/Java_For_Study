07bb72216a9fc970f3514f11b67fa9ceb19fe8b1
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2023-03-08 18:17:04 +0400

    Update HumanTest.java

--- StartOfReflection/Ermak_Block_7.1/HumanTest.java
+++ StartOfReflection/Ermak_Block_7.1/HumanTest.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package tinkoff;
-
-/**
- *
- * @author Ilia_Dukhov
- */
 public class HumanTest {
     public void check(Object obj) throws Exception{
         if(!(obj instanceof Human)) throw new Exception();
