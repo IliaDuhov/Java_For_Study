0535d4b349d71967e291209e92aa64ae8941704a
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2023-03-12 13:45:11 +0400

    Update Testik.java

--- StartOfReflection/Annotaions_and_tests/Testik.java
+++ StartOfReflection/Annotaions_and_tests/Testik.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package Reflection;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
  *
- * @author Ilia_Dukhov
+ * One-element annotation
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Testik {
