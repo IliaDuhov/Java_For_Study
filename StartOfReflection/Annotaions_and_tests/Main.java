82f1c0d6a490d3713184702979f49b82a796999e
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2023-03-12 13:46:53 +0400

    Update Main.java

--- StartOfReflection/Annotaions_and_tests/Main.java
+++ StartOfReflection/Annotaions_and_tests/Main.java
@@ -11,30 +11,30 @@ import java.util.Scanner;
 
 public final class Main {
     public static void main(String[] args) throws Throwable {
-        C c = new C();
-        System.out.println(fieldCollection(c));
-        Human h = new Human(20, 180);
-        validate(h, HumanTest.class);
-        Constructor[] carr = h.getClass().getDeclaredConstructors();
+//         C c = new C();
+//         System.out.println(fieldCollection(c));
+//         Human h = new Human(20, 180);
+//         validate(h, HumanTest.class);
+//         Constructor[] carr = h.getClass().getDeclaredConstructors();
         //Object ob = carr[0].newInstance(40, 200);//use the first constructor of the Human class
         //System.out.println(Arrays.asList(A.class.getAnnotatedInterfaces()));//get annotations of A class (comment @Target in annotions of Class A to check the impact of method)
         tester(A.class);
     }
     
-    public static List fieldCollection(Object obj){
-        Class clz = obj.getClass();
-        List<Field> lst = new ArrayList(Arrays.asList(clz.getDeclaredFields()));
-        while(clz.getSuperclass()!=Object.class){
-            clz = clz.getSuperclass();
-            lst.addAll(Arrays.asList(clz.getDeclaredFields()));
-        }
-        return lst;
-    }
-    public static void validate(Object object, Class testClass) throws Throwable{
-        for(Method method: testClass.getDeclaredMethods()){
-            method.invoke(testClass.newInstance(), object);
-        }
-    }
+//     public static List fieldCollection(Object obj){
+//         Class clz = obj.getClass();
+//         List<Field> lst = new ArrayList(Arrays.asList(clz.getDeclaredFields()));
+//         while(clz.getSuperclass()!=Object.class){
+//             clz = clz.getSuperclass();
+//             lst.addAll(Arrays.asList(clz.getDeclaredFields()));
+//         }
+//         return lst;
+//     }
+//     public static void validate(Object object, Class testClass) throws Throwable{
+//         for(Method method: testClass.getDeclaredMethods()){
+//             method.invoke(testClass.newInstance(), object);
+//         }
+//     }
     @SafeVarargs //отключает warning на heap polution
     public static<T> void test(T...arr){
         
