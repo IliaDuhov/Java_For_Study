95a918e679d2ceea91af2a400341813ecb6ef740
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2023-03-12 13:45:56 +0400

    Update Test.java

--- StartOfReflection/Annotaions_and_tests/Test.java
+++ StartOfReflection/Annotaions_and_tests/Test.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Reflection;
-
 /**
  *
- * @author Ilia_Dukhov
+ * class to test methods in A.class
  */
 public class Test {
     public static int intTest(int x){return 3;}
