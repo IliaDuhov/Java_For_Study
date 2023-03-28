caf0f17e55e003cbfdad9e3bd0ae9a6ff9a34b64
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2023-03-29 00:00:23 +0400

    Update Config.java

--- Spring/Autowiered/Config.java
+++ Spring/Autowiered/Config.java
@@ -17,4 +17,12 @@ public class Config {
     public String str(){
         return "Hello, world";
     }
+//    @Bean
+//    public String str123(){
+//        return "Bullshit";
+//    }
+      //если в сеттере внедрять str123, то в поле внедрится Bullshit, так как методы последние инициализируют поля
+     //порядок внедрения 1. Конструткоры
+                    //   2. Поля
+                    //   3. Сеттеры
 }
